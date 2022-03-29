0cf0baf83363338b6e331c1b514e7b3299c04671
Havir <saffronkrzysztof@gmail.com>
2022-03-29 12:09:24 +0200

    css complete
    Signed-off-by: Havir <saffronkrzysztof@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div className='wrapper' id='drum-machine'>
+        <div className='buttons' id='display'>
+          <div className='button drum-pad'>
+            <div>1</div>
+            <audio controls autoplay hidden>
+
+            </audio>
+          </div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+          <div className='button drum-pad'><div>1</div></div>
+        </div>
+
+        <div className='options'>
+          <div className='powerBox'>
+            <p>Power</p>
+            <div className='powerSwitch'>
+            <label className='switch'>
+              <input type='checkbox' />
+              <span className='slider'></span>
+            </label>
+            </div>
+          </div>
+          <div className='textBox'>
+            <div className='textContent'>
+            Volume
+            </div>
+          </div>
+
+          <div className='volumeBox'>
+            <div className='volumeSlider'>
+            <input type='range'min='0'max='100'  className='slider' id='slider'/>
+            </div>
+          </div>
+
+          <div className='bankBox'>
+            <p>Bank</p>
+            <div className='bankSwitch'>
+            <label className='switch'>
+              <input type='checkbox' />
+              <span className='slider'></span>
+            </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
